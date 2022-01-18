4d9165ee81c9767fcb7eab8ed58d8bacaf9f3b0b
Nayoung7 <95176265+Nayoung7@users.noreply.github.com>
2022-01-18 20:41:45 +0900

    Rstudy-01.03

--- exam01.R
+++ exam01.R
@@ -1,99 +1,115 @@
-id <- c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)
-grade <- c(1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5)
-mid <- c(20,23,26,11,22,29,34,37,15,14,26,15,24,24,33,19,11,27,34,21)
-fin <- c(33,39,21,11,16,12,30,29,26,25,27,25,11,10,33,25,18,33,21,34)
+# 주석 : 프로그램은 번역하지 않고 넘어가는 부분
 
-install.packages("dplyr")
-library(dplyr)
+# 변수 : 데이터를 저장할 수 있는 임시적인 공간
 
-score <- data.frame(id, grade, mid, fin)
+# num이라는 이름의 변수 안에 정수 100을 저장
+num <- 100
+num
 
-# 데이터를 그룹별로 묶어주기
-# 각 반별로 묶어주는 방법 group_by('컬럼')
-# A tibble : 20개의 행 X 4개의 열
-# Groups: grade[5] >> grade를 기준으로 5개의 그룹이 나온다
-# 단순히 group_by()만 사용하지 않고 summarise() 함수와 같이 사용된다.
-# summarise() : 요약통계량을 볼 수 있는 함수
-# group_by() 함수를 쓰고 나서는 일반 연산함수 쓸수 없고 summarise 함수를 통해서 연산을 해야한다.
-score %>% group_by(grade)
-score %>% group_by(grade) %>% summarise(m_mid = mean(mid))
+# 함수 : 데이터를 넣으면 특정한 기능을 수행해 데이터를 가공하여 값을 출력
+sum(1,50,60,90,99,87,65,12,33,54,15)
 
-score %>% group_by(grade) %>% summarise(m_mid = max(mid))
+# 패키지 : 함수들을 모아놓은 묶음
 
+# 변수의 데이터 타입
+# 숫자형 : 정수, 실수 등의 숫자들을 저장할 수 있는 형태
+num1 <- 10.3
+# 문자형 : 알파벳, 한글 등 문자 데이터를 저장할 수 있는 형태
+text1 <- "Hello World!"
+text2 <- 'Hello World!'
+# 논리형 : 참, 거짓을 저장할 수 있는 형태
+isCheck <- TRUE
+isCheck <- FALSE
+isCheck <- T
+isCheck <- F
+# NA & NULL 형 : 데이터의 상황을 저장할 수 있는 형태
+# NA -> 결측치 : 데이터는 있는데 정의되어 있지 않음
+# NULL -> 비어있는 값
+sum(1,2,3,4,5,NA)
+sum(1,2,3,4,5,NULL)
 
-# dataframe 합치기
-mid <- data.frame(id=c(101,102,103,104,105),mid=c(60,70,80,90,85))
-fin <- data.frame(id=c(103,104,105,106,107), fin=c(70,83,65,50,75))
+name<-'박병관'
+name
+name<-'황정민'
+name
 
-# innerJoin 내부 공통으로 가지고 있는 행만 결합
-inner_join(mid, fin, by="id")
+# 자료구조 : 대량의 데이터를 효과적으로 관리할 수 있는 방법
+# 벡터(Vector) : R에서 가장 기본이 되는 데이터 구조
+#                동일한 유형의 데이터가 1차원으로 구성 (배열과 비슷)
+# 숫자형 벡터
+v_num <- c(10,15,20)
+v_num
+# 문자형 벡터
+v_text <- c('사과', '딸기', '참외', '수박', '귤', '복숭아')
+v_text
+# 논리형 벡터
+v_log <- c(T,F,T,F,T,T)
+v_log
 
-# full_join 두개의 데이터 프레임을 합함
-full_join(mid, fin, by="id")
+v_text[5]
 
-# left_join 왼쪽을 기준으로 병합
-left_join(mid, fin, by="id")
+# 일률적인 벡터 생성(순차적으로 진행하는것)
+v1 <- c(1,2,3,4,5,6,7,8,9,10)
+v2 <- seq(1,10)
+v3 <- 1:10
+v4 <- seq(1,100, by = 4)
+v5<- seq(1,100, length.out = 4)
+v5
 
-# right_join 오른쪽을 기준으로 병합
-right_join(mid, fin, by="id")
+# 벡터의 반복
+x <- seq(1,3)
+rep(x, times=2)
+rep(x, each = 2)
 
-# 행 기준으로 데이터 합치기 : bind_rows(), 컬럼 이름과 갯수가 같아야함
-mid2 <- data.frame(id=c(106,107), mid=c(85,77))
-mid
-mid2
+# 실습
+foods <- c('간짜장', '족발', '굴보쌈', '참돔', '감자탕', '순대국밥')
 
-bind_rows(mid,mid2)
+# 문제1. 간짜장 굴보쌈 감자탕을 콘솔에 출력하시오
+c(foods[1], foods[3], foods[5])
+foods[c(1,3,5)]
+foods[seq(1,6,by=2)]
 
-# 실습 항공데이터 분석하기
-install.packages("hflights")
-library(hflights)
-dim(hflights)
-View(hflights)
+foods[seq(2,6,by=2)]
 
-# 비행기 번호판별 가장 오래걸린 출발시간을 출력하시오
-hflights %>% group_by(TailNum) %>% summarise(max(DepTime, na.rm=T))
+f <- ""
+for(i in 1:6){
+  if (i %% 2 == 0){
+    f <- f + foods[i]
+  }
+}
+print(f)
 
-# 결항건수가 가장 많았던 날 알아내기
-# 1. 결항사유별 건수를 출력하세요
-hflights %>%group_by(CancellationCode) %>% summarise(n=n())
+# 불리언 인덱싱 > true에 해당되는 값만 꺼내기
+x <- seq(1,20,by=3)
 
-# 결항사유가 날씨('B') 이거나 기류('C')상황인 데이터만 출력하시오
-# 그 다음 월별 건수 출력
-hflights %>%  filter(CancellationCode == "B" | CancellationCode =="C") %>% group_by(Month) %>% summarise(n=n()) %>% arrange(desc(n))
+# x의 요소 중 5보다 큰 값만 꺼내기
+x[x > 5]
+# x의 요소 중 7과 같은 값만 꺼내기
+x[x == 7]
 
-# 월 > 일 순으로 그룹화해서 출력하기
-# 2월 1,3,4일 : 
-hflights %>%  filter(CancellationCode == "B" | CancellationCode =="C") %>% group_by(Month, DayofMonth) %>% summarise(n=n()) %>% arrange(desc(n))    
+# 특정 요소를 제거하고 보기
+x[-c(1,2,3)]
 
-list.files()
-library(readxl)
+# 특정 위치의 요소값을 변경
+x[c(1,2)]
+x[c(1,2)] <- 99
+x
 
-score <- read_excel("score.xlsx")
-score
+# 문제1. 100부터 300까지 숫자중 3씩 증가하는 숫자 v1을 출력하세요
+v1 <- seq(100,300,by=3)
+v1
 
-# na(결측치)를 파악하는 함수 : is.na(df)
-is.na(score)
-# True와 False의 개수를 출력하시오
-table(is.na(score))
-# 속성(컬럼)별 na의 개수 출력
-summary(score)
+# 문제2. 20번째부터 30번째에 있는 숫자만 출력하세요
+v1[20:30]
 
-# filter를 활용하여 mid안에 결측치가 없는 행만 출력
-score %>% filter(!is.na(mid)) # true -> false, false -> true로 바꿔줘야함
+# 문제3. 20번째부터 30번째에 있는 숫자를 제외하고 출력하세요
+v1[-c(20:30)]
+v1[-seq(20:30)]
+v1[seq(20,30)]
+v2 = v1[-(20:30)]
+v2
 
-# filter를 활용하여 전체 결측치가 있는 행을 제거
-score %>% filter(!is.na(mid) & !is.na(fin) & !is.na(assign) & !is.na(att))
+# 문제4. v1 중 200보다 큰 수를 출력하세요.
+v1[v1 > 200]
 
-score %>% na.omit()
-
-median(score$mid, na.rm = T)
-
-# 문제 mid안에 결측치가 있다면 중앙값으로 대체해주세요.
-score$mid <- ifelse(is.na(score$mid),median(score$mid, na.rm = T),score$mid)
-score$mid
-
-boxplot(score$mid)
-
-# 문제 mid안에 값이 100점 초과라면 중앙값으로 대체해주세요
-score$mid <- ifelse(score$mid > 100, 25, score$mid)
-score$mid
+v1[c(20:30, by=3)]
